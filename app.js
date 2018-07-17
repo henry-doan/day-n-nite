367e65da96d3cb7ed3815ec08ccc864d373aa449
Henry Doan <henrydoan96@gmail.com>
2018-07-16 20:32:53 -0600

    refactor js to ternarys

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 $(document).ready(function() {
 
   $('#orb').click(function() {
-    if($(this).hasClass('sun')) {
-      $(this).removeClass('sun').addClass('moon');
-    } else {
-      $(this).removeClass('moon').addClass('sun');
-    }
 
-    if($('#sky').hasClass('day')) {
-      $('#sky').removeClass('day').addClass('night');
-    } else {
-      $('#sky').removeClass('night').addClass('day');
-    }
+    // Changes the orb
+    var orb = $(this);
+    var orbCurClass = orb.attr('class');
+    var orbNewClass = (orbCurClass == 'sun') ? 'moon' : 'sun';
+    orb.removeClass().addClass(orbNewClass);
 
-    if($('#moonspot1').hasClass('visible')) {
-      $('#moonspot1').removeClass('visible');
-    } else {
-      $('#moonspot1').addClass('visible');
-    }
+    // changes the sky
+    var sky = $('#sky');
+    var skyCurClass = sky.attr('class');
+    var skyNewClass = (skyCurClass == 'day') ? 'night' : 'day';
+    sky.removeClass().addClass(skyNewClass);
 
-    if ($('#moonspot2').hasClass('visible')) {
-      $('#moonspot2').removeClass('visible');
-    } else {
-      $('#moonspot2').addClass('visible');
-    }
+    // changes the craters 
+    var crater1 = $('#crater1');
+    var crat1CurClass = crater1.attr('class');
+    var crat1NewClass = (crat1CurClass == 'visible') ? '' : 'visible';
+    crater1.removeClass().addClass(crat1NewClass);
 
-    if ($('#moonspot3').hasClass('visible')) {
-      $('#moonspot3').removeClass('visible');
-    } else {
-      $('#moonspot3').addClass('visible');
-    }
+    var crater2 = $('#crater2');
+    var crat2CurClass = crater2.attr('class');
+    var crat2NewClass = (crat2CurClass == 'visible') ? '' : 'visible';
+    crater2.removeClass().addClass(crat2NewClass);
+
+    var crater3 = $('#crater3');
+    var crat3CurClass = crater3.attr('class');
+    var crat3NewClass = (crat3CurClass == 'visible') ? '' : 'visible';
+    crater3.removeClass().addClass(crat3NewClass);
   })
 })
